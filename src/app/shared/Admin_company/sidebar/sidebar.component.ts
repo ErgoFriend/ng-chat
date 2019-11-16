875f248d5f48f402b4e6f6c4d471aea3b9a9864b
ErgoFriend <erg.friend@gmail.com>
2019-11-16 17:05:25 +0900

    add bills comp

--- src/app/shared/Admin_company/sidebar/sidebar.component.ts
+++ src/app/shared/Admin_company/sidebar/sidebar.component.ts
@@ -8,33 +8,40 @@ export interface RouteInfo {
 }
 
 export const ROUTES: RouteInfo[] = [
-  {
-    path: "/companyAdmin/profile",
-    title: "Company Profile",
-    icon: "nc-single-02",
-    class: ""
-  },
-  {
-    path: "/companyAdmin/articlelist",
-    title: "Article List",
-    icon: "nc-bullet-list-67",
-    class: ""
-  },
+         {
+           path: "/companyAdmin/profile",
+           title: "Company Profile",
+           icon: "nc-single-02",
+           class: ""
+         },
+         {
+           path: "/companyAdmin/articlelist",
+           title: "Article List",
+           icon: "nc-bullet-list-67",
+           class: ""
+         },
 
-  {
-    path: "/companyAdmin/userlist",
-    title: "UserList",
-    icon: "nc-check-2",
-    class: ""
-  },
-  // { path: "/table", title: "Table List", icon: "nc-tile-56", class: "" },
-  {
-    path: "/messenger",
-    title: "Messages",
-    icon: "nc-single-copy-04",
-    class: ""
-  }
-];
+         {
+           path: "/companyAdmin/userlist",
+           title: "UserList",
+           icon: "nc-check-2",
+           class: ""
+         },
+         // { path: "/table", title: "Table List", icon: "nc-tile-56", class: "" },
+         {
+           path: "/messenger",
+           title: "Messages",
+           icon: "nc-single-copy-04",
+           class: ""
+         },
+
+         {
+           path: "/companyAdmin/bills",
+           title: "お支払い",
+           icon: "nc-check-2",
+           class: ""
+         }
+       ];
 
 @Component({
   moduleId: module.id,
